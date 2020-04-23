cb3d66585585e187ec821a125bbaafee6b1bcedf
Miguel Antenor de Oliveira <miguelantoliv@gmail.com>
2020-04-23 19:09:53 -0300

    - First commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Primeiro Projeto
+> Aplicativo desenvolvido seguindo o curso
+> https://www.udemy.com/course/curso-completo-flutter-app-android-ios/
+### Ferramentas utilizadas
+* Dart
+* Flutter
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: MyHomePage(title: 'Primeiro Projeto'),
+    );
+  }
+
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int _counter = 0;
+  final StreamController<int> _streamController = StreamController<int>();
+
+  void _incrementCounter() {
+    _counter++;
+    _streamController.sink.add(_counter);
+  }
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder<int>(
+              stream: _streamController.stream,
+              initialData: _counter,
+              builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                return Text(
+                  '${snapshot.data}',
+                  style: Theme.of(context).textTheme.display1,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
